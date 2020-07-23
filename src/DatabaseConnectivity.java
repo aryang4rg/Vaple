060ae8c6609961338d7b006bb62fd39388727d72
slayerofthend <42659052+slayerofthend@users.noreply.github.com>
2020-07-22 17:49:51 -0700

    Added User

---
+++ src/DatabaseConnectivity.java
@@ -0,0 +1,25 @@
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+
+public class DatabaseConnectivity
+{
+    public static void main(String[] args) throws UnknownHostException
+    {
+        MongoClient mongoClient = new MongoClient();
+        DB database = mongoClient.getDB("test");
+        DBCollection collection = database.getCollection("epic");
+        List<Integer> books = Arrays.asList(27464, 747854);
+        DBObject person = new BasicDBObject("_id", "jo")
+                .append("name", "Jo Bloggs")
+                .append("address", new BasicDBObject("street", "123 Fake St")
+                        .append("city", "Faketon")
+                        .append("state", "MA")
+                        .append("zip", 12345))
+                .append("books", books);
+        collection.insert(person);
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,105 @@
+import java.util.ArrayList;
+
+public class User
+{
+    String name, email, id, password, location_country, location_state, location_city, description, token;
+    ArrayList<String> followers, following, activities;
+
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLocation_country() {
+        return location_country;
+    }
+
+    public void setLocation_country(String location_country) {
+        this.location_country = location_country;
+    }
+
+    public String getLocation_state() {
+        return location_state;
+    }
+
+    public void setLocation_state(String location_state) {
+        this.location_state = location_state;
+    }
+
+    public String getLocation_city() {
+        return location_city;
+    }
+
+    public void setLocation_city(String location_city) {
+        this.location_city = location_city;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public ArrayList<String> getFollowers() {
+        return followers;
+    }
+
+    public void setFollowers(ArrayList<String> followers) {
+        this.followers = followers;
+    }
+
+    public ArrayList<String> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(ArrayList<String> following) {
+        this.following = following;
+    }
+
+    public ArrayList<String> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(ArrayList<String> activities) {
+        this.activities = activities;
+    }
+}
