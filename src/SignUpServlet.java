5603fb117732b01a168a1bc29507ca5be7e96f7a
destame <destame12345@gmail.com>
2020-07-22 22:07:32 -0700

    signup check

--- src/SignUpServlet.java
+++ src/SignUpServlet.java
@@ -7,6 +7,26 @@ import java.io.IOException;
 
 public class SignUpServlet implements AjaxHandler
 {
+	public boolean isValid(String k){
+		if(k == null || k.length() == 0)
+			return false;
+		return true;
+	}
+
+	public boolean isValidEmail(String email){
+		int pi = email.lastIndexOf('.');
+		int ai = email.indexOf('@');
+
+		if(ai < 1 || pi <= ai + 1 || pi + 1 >= email.length)
+			return false;
+		else
+			return true;
+	}
+
+	public boolean isValidPassword(String k){
+		return k.length() <= 32;
+	}
+
     @Override
     public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         JsonNode node = Json.parse(req.getReader());
@@ -20,6 +40,22 @@ public class SignUpServlet implements AjaxHandler
         //TODO make token, verify fields exist
        // User user = new User(name,email,password, location_country, location_state, location_city, );
 
+		if(!isValid(email) || !isValid(name) || !isValid(password) || !isValid(location_country) ||
+			!isValid(location_state) || !isValid(location_city)){
+				resp.getWriter().println("{\"token\": null}");
 
+				return;
+			}
+		if(!isValidEmail(email)){
+			resp.getWriter().println("{\"token\": null, \"error\": \"Invalid email\"}");
+
+			return;
+		}
+
+		if(!isValidPassword(password)){
+			resp.getWriter().println("{\"token\": null, \"error\": \"Password must be less than 32 characters\"}");
+
+			return;
+		}
     }
 }
