021a337a2da3c36685c6c5574a566d20dba1265b
xBobOmb <lordpumpkinpieness@gmail.com>
2020-07-25 00:01:05 -0700

    fixed spam emails

---
+++ src/main/java/databaseobject/Challenge.java
@@ -0,0 +1,14 @@
+package databaseobject;
+
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+
+public class Challenge extends Activity
+{
+    public Challenge(ArrayList<ObjectId> challengers)
+    {
+
+        super();
+    }
+}
