361bc07d12424d1f80b32b99b8765a25c24ffed0
slayerofthend <42659052+slayerofthend@users.noreply.github.com>
2020-07-25 19:44:48 -0700

    no errors

--- src/main/java/ajaxhandler/fulfiller/ExploreFeedHandler.java
+++ src/main/java/ajaxhandler/fulfiller/ExploreFeedHandler.java
@@ -3,32 +3,63 @@ package ajaxhandler.fulfiller;
 import ajaxhandler.AjaxHandler;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.mongodb.DBObject;
+import databaseobject.Activity;
+import databaseobject.Club;
 import databaseobject.User;
+import main.DatabaseConnectivity;
+import org.bson.types.ObjectId;
+import util.Util;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static databaseobject.Activity.getActivitiesByTag;
 
 public class ExploreFeedHandler implements AjaxHandler
 {
 
-     boolean isPage;
-
-     public ExploreFeedHandler(boolean isPage)
-     {
-         this.isPage = isPage;
-     }
-
     @Override
     public int service(HttpServletRequest req, HttpServletResponse resp, JsonNode request, ObjectNode response, String[] uriSplit, User user) throws ServletException, IOException
     {
-        return 400;
+        String tag = Util.asText(request.get("tag"));
+        tag = Util.trimAndnullIfSpecialCharacters(tag);
+        if (tag != null)
+        {
+            List<DBObject> activities = DatabaseConnectivity.getAllObjectsByProperty(tag, DatabaseConnectivity.ACTIVITYCOLLECTION);
+            List<DBObject> clubs = DatabaseConnectivity.getAllObjectsByProperty(tag, DatabaseConnectivity.CLUBCOLLECTION);
+            List<DBObject> users = DatabaseConnectivity.getAllObjectsByProperty(tag, DatabaseConnectivity.ACCOUNTCOLLECTION);
+
+            ObjectNode activityNode = Util.createObjectNode();
+            ObjectNode clubNode = Util.createObjectNode();
+            ObjectNode userNode = Util.createObjectNode();
+
+            for (DBObject obj: activities)
+            {
+                ObjectId id = (ObjectId) obj.get(ID);
+                Activity myActivity = (Activity)Activity.databaseConnectivity().getFromInfoInDataBase(ID, id);
+                if (myActivity != null)
+                {
+                    ObjectNode innerNode = Util.createObjectNode();
+                    innerNode.put("id", ((ObjectId)myActivity.get("id")).toHexString());
+                    //innerNode.put("name", ())
+
+                    activityNode.put( ((ObjectId)myActivity.get("id")).toHexString(), innerNode);
+                }
+            }
+
+
+        }
+        return 0;
     }
 
     @Override
     public boolean isPage()
     {
-        return isPage;
+        return true;
     }
 }
